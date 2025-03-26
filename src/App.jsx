2893d8dc2b51a41395c4d54760ba2c89a8ceca01
pavankumar-vh <pavankumarvh2006@gmail.com>
2025-03-26 09:36:02 +0530

    FIles Added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TeamMemberCard from "./components/TeamMemberCard.jsx";
+
+function App() {
+  return (
+    <div>
+      <TeamMemberCard name={Harish} jobtitle={KalviumMentor}/>
+      <TeamMemberCard name={Josh} jobtitle={KalviumMentor}/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/TeamMemberCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function TeamMemberCard({name,jobtitle}){
+    <div class="cardpanel">
+        <h1>Name : {name}</h1>
+        <h1>Job Title : {jobtitle}</h1>
+    </div>
+}
+
+export default TeamMemberCard;
